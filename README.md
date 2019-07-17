f7fae6d568184b21fc8101adf7cf5e992e402291
n-a-o-k-i <nao.rami.1124@i.softbank.jp>
2019-07-18 02:50:01 +0900

    first

---
+++ README.md
@@ -0,0 +1,22 @@
+# README
+
+This is application that can chat with asynchronous bidirectional communication.
+
+* Ruby version  
+2.6.3
+
+* Rails version  
+5.2.3
+
+* Used technology  
+ActionCable
+
+* Used gem  
+Devise
+
+* Branch  
+master: LINE style application  
+devise+action_cable: Not LINE style  
+
+Here's an article on this.(Language: Japanese)  
+https://qiita.com/eRy-sk/items/4c4e983e34a44c5ace27
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :messages
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
